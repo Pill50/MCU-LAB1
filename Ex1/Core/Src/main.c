14a7eb8803ad727f1e9a7cb79fe4a18af92bbadd
Pill50 <tranvinhphuc251213@gmail.com>
2022-09-29 01:33:00 +0700

    ex7 done

--- Ex1/Core/Src/main.c
+++ Ex1/Core/Src/main.c
@@ -103,8 +103,18 @@ int main(void)
                   GPIO_PIN_15, SET);
       HAL_GPIO_WritePin(GPIOA, ledPin[n], RESET);
   }
+
+  void clearAllClock(){
+      HAL_GPIO_WritePin (GPIOA, GPIO_PIN_4 | GPIO_PIN_5
+                  | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8
+                  | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11
+                  | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14
+                  | GPIO_PIN_15,  SET);
+  }
+
   int n = 0;
   while (1) {
+	  clearAllClock();
       testLed(n);
       n++;
       if(n >= 12) n = 0;
